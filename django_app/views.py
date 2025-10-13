03c5372745f8974ad5e65260befb57113c1031e1
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2025-10-13 15:45:43 +0500

    finished

---
+++ django_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import *
+
+def index(request):
+    categories = Category.objects.all()
+    news = News.objects.all()
+    return render(request, 'index.html', {'categories': categories, 'news': news})
