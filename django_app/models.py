12b70d909a2b6ac94fc4bd3917fc16000e035747
ZuhriddinRW <shodmonovzuhriddin2009@gmail.com>
2025-10-14 18:44:52 +0500

    formatted

--- django_app/models.py
+++ django_app/models.py
@@ -1,24 +1,21 @@
 from django.db import models
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=100)
+class Category ( models.Model ) :
+    title = models.CharField ( max_length=100 )
 
-
-    def __str__(self):
+    def __str__(self) :
         return self.title
 
 
+class News ( models.Model ) :
+    title = models.CharField ( max_length=100, verbose_name='sarlovha' )
+    content = models.TextField ( blank=True, null=True, verbose_name='Text' )
+    created_at = models.DateTimeField ( auto_now_add=True, verbose_name='add_date' )
+    updated_at = models.DateTimeField ( auto_now=True )
+    photo = models.ImageField ( upload_to='photo/%Y/%m/%d', blank=True, null=True )
+    bool = models.BooleanField ( default=False, verbose_name='Bool' )
+    category = models.ForeignKey ( Category, on_delete=models.CASCADE, default=1 )
 
-
-class News(models.Model):
-    title = models.CharField(max_length=100, verbose_name='sarlovha')
-    content = models.TextField(blank=True, null=True, verbose_name='Text')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='add_date')
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photo/%Y/%m/%d' , blank=True, null=True)
-    bool = models.BooleanField(default=False, verbose_name='Bool')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-
-    def __str__(self):
+    def __str__(self) :
         return self.title
--- django_project/settings.py
+++ django_project/settings.py
@@ -3,14 +3,12 @@ import os
 
 from django.conf.global_settings import MEDIA_URL
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path ( __file__ ).resolve ().parent.parent
 SECRET_KEY = 'django-insecure-_eliyiphgy!wzzugo!!j^e@zyy5_(ie9n)eudrwm9(1owyo1b+'
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,11 +34,11 @@ ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
+        'BACKEND' : 'django.template.backends.django.DjangoTemplates',
+        'DIRS' : [],
+        'APP_DIRS' : True,
+        'OPTIONS' : {
+            'context_processors' : [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -51,30 +49,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME' : BASE_DIR / 'db.sqlite3',
     }
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME' : 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME' : 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME' : 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME' : 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 LANGUAGE_CODE = 'en-us'
 
@@ -84,10 +79,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join ( BASE_DIR, 'static' )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join ( BASE_DIR, 'media' )
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
